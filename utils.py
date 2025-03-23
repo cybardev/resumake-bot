512b76db89aeda0bdbee886db8e127c75fc43547
cybardev <50134239+cybardev@users.noreply.github.com>
2025-03-23 18:52:12 -0300

    add resume generation API call using aiohttp

--- utils.py
+++ utils.py
@@ -1,8 +1,27 @@
 import asyncio
 import contextlib
+import logging
 import os
 
 from aiohttp import web
+import aiohttp
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+async def generate_resume(filename):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            "https://resumake.cybar.dev/resume/",
+            data={"resume": open(f"{filename}.yaml", "rb")},
+        ) as res:
+            if res.status == 200:
+                pdf_content = await res.read()  # Read response content as bytes
+                with open(f"{filename}.pdf", "wb") as pdf_file:
+                    pdf_file.write(pdf_content)
+            else:
+                logger.error(f"Failed to generate resume. Error {res.status}")
 
 
 async def _web_handler(request):
